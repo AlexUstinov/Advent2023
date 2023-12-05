3cb3399644d523a9e8fa3e4a0c5f28138d752990
Aleksandr Ustinov <ustinovas@gmail.com>
2023-12-05 08:52:50 +0000

    Solve 2nd problem of the 4th day.

--- src/day_04.rs
+++ src/day_04.rs
@@ -21,6 +21,35 @@ impl Solution {
         }
         total_score
     }
+
+    pub fn count_card_copies(lines: Vec<String>) -> i32 {
+        use std::collections::{HashMap, HashSet};
+
+        let mut card_counters = HashMap::<i32, i32>::new();
+        for game in lines {
+            let (card_id, win_nums, player_nums) = game.split_once(':')
+                .map(|(card, nums)| { 
+                    let (win_nums, player_nums) = nums.split_once('|').unwrap();
+                    (card[5..].trim().parse::<i32>().unwrap(),
+                        win_nums.trim().split(' ').filter(|num| num.len()>0),
+                        player_nums.trim().split(' ').filter(|num| num.len()>0))
+                })
+                .unwrap();            
+            let card_count = card_counters.entry(card_id).or_insert(0);
+            *card_count += 1;
+            let card_count = *card_count;
+            let mut win_nums_set = HashSet::new();
+            win_nums_set.extend(win_nums);
+            let match_count = player_nums.filter(|num| win_nums_set.contains(num)).count() as i32;
+            if match_count>0 {
+                for next_card_id in (1..=match_count).map(|d| card_id + d) {
+                    *card_counters.entry(next_card_id).or_insert(0) += card_count;
+                }
+            }
+        }
+
+        card_counters.values().sum()
+    }
 }
 
 #[cfg(test)]
@@ -36,4 +65,10 @@ mod tests {
         println!("{result:?}");
     }
 
+    #[tokio::test]
+    async fn solve2() {
+        let lines = load_lines("day_04.txt").await.unwrap();
+        let result = Solution::count_card_copies(lines);
+        println!("{result:?}");
+    }
 }
