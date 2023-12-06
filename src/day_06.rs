4a9aa2d6f6f5d812479e4ff86694e022d4714791
Aleksandr Ustinov <ustinovas@gmail.com>
2023-12-06 02:13:06 +0000

    Solve the 2nd problem of the 6th day.

--- src/day_06.rs
+++ src/day_06.rs
@@ -24,6 +24,50 @@ impl Solution {
         aggregated_num_ways
     }
 
+    pub fn num_ways_to_win_big_race(lines: Vec<String>) -> i64 {
+        let time = lines[0].chars().filter(|c| c.is_ascii_digit()).collect::<String>().parse::<i64>().unwrap();
+        let distance = lines[1].chars().filter(|c| c.is_ascii_digit()).collect::<String>().parse::<i64>().unwrap();
+
+        let (mut l, mut r) = (0, time);
+        while l < r {
+            let start_time = l + (r-l)/2;
+            let speed = start_time;
+            let d1 = (time - start_time)*speed;
+            let d2 = (time - start_time - 1)*(speed+1);
+            if d2 > d1 {
+                l = start_time + 1;
+            } else {
+                r = start_time;
+            }
+        }
+        let best_start_time = l;
+        let (mut l, mut r) = (0, best_start_time+1);
+        while l < r {
+            let start_time = l + (r-l)/2;
+            let speed = start_time;
+            let d = (time - start_time)*speed;
+            if d<=distance {
+                l = start_time + 1;
+            } else {
+                r = start_time;
+            }
+        }
+        let win_range_start = l;
+        let (mut l, mut r) = (best_start_time+1, time);
+        while l < r {
+            let start_time = l + (r-l)/2;
+            let speed = start_time;
+            let d = (time - start_time)*speed;
+            if d>distance {
+                l = start_time + 1;
+            } else {
+                r = start_time;
+            }
+        }
+        let win_range_end = l;
+        win_range_end - win_range_start
+    }
+
     
 }
 
@@ -37,6 +81,13 @@ mod tests {
     async fn solve1() {
         let lines = load_lines("day_06.txt").await.unwrap();
         let result = Solution::num_ways_to_win(lines);
+        println!("{result:?}");
+    }
+
+    #[tokio::test]
+    async fn solve2() {
+        let lines = load_lines("day_06.txt").await.unwrap();
+        let result = Solution::num_ways_to_win_big_race(lines);
         println!("{result:?}");
     }
 }
