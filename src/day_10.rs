cc9ed7c20f91147aafe788538edfd63cf7579f36
Aleksandr Ustinov <ustinovas@gmail.com>
2023-12-10 04:52:56 +0000

    Add 10th day code skeleton.

---
+++ src/day_10.rs
@@ -0,0 +1,21 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn solve1(lines: Vec<String>) -> i32 {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::util::load_lines;
+    use tokio;
+
+    #[tokio::test]
+    async fn solve1() {
+        let lines = load_lines("day_10.txt").await.unwrap();
+        let result = Solution::solve1(lines);
+        println!("{result:?}");
+    }
+}
