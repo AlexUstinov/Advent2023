002e30211704935c305aa5b7eb11dc795e8c03d6
Aleksandr Ustinov <ustinovas@gmail.com>
2023-12-15 08:15:59 +0000

    Use Floyd's Tortoise and Hare algorithm to fix the 14/2 solution.

--- src/day_14.rs
+++ src/day_14.rs
@@ -21,18 +21,19 @@ impl Solution {
         total_load
     }
 
-    pub fn calculate_load_in_cycles(mut lines: Vec<Vec<u8>>, num_cycles: i32) -> i32 {
-        let (n, m) = (lines.len(), lines[0].len());
-        for cycle in 0..num_cycles {
+    pub fn calculate_load_in_cycles(mut platform: Vec<Vec<u8>>, num_rotations: i32) -> i32 {
+        fn rotate(mut platform: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+            let (n, m) = (platform.len(), platform[0].len());
+
             // tilt north
             for col in 0..m {
                 let mut min_pos = 0;
                 for row in 0..n {
-                    match lines[row][col] {
+                    match platform[row][col] {
                         b'#' => min_pos = row + 1,
                         b'O' => {
-                            lines[row][col] = b'.';
-                            lines[min_pos][col] = b'O';
+                            platform[row][col] = b'.';
+                            platform[min_pos][col] = b'O';
                             min_pos += 1;
                         },
                         _ => continue
@@ -43,11 +44,11 @@ impl Solution {
             for row in 0..n {
                 let mut min_pos = 0;
                 for col in 0..m {
-                    match lines[row][col] {
+                    match platform[row][col] {
                         b'#' => min_pos = col + 1,
                         b'O' => {
-                            lines[row][col] = b'.';
-                            lines[row][min_pos] = b'O';
+                            platform[row][col] = b'.';
+                            platform[row][min_pos] = b'O';
                             min_pos += 1;
                         },
                         _ => continue
@@ -58,11 +59,11 @@ impl Solution {
             for col in 0..m {
                 let mut min_pos = n-1;
                 for row in (0..n).rev() {
-                    match lines[row][col] {
+                    match platform[row][col] {
                         b'#' => min_pos = row.saturating_sub(1),
                         b'O' => {
-                            lines[row][col] = b'.';
-                            lines[min_pos][col] = b'O';
+                            platform[row][col] = b'.';
+                            platform[min_pos][col] = b'O';
                             min_pos = min_pos.saturating_sub(1);
                         },
                         _ => continue
@@ -73,49 +74,51 @@ impl Solution {
             for row in 0..n {
                 let mut min_pos = m-1;
                 for col in (0..m).rev() {
-                    match lines[row][col] {
+                    match platform[row][col] {
                         b'#' => min_pos = col.saturating_sub(1),
                         b'O' => {
-                            lines[row][col] = b'.';
-                            lines[row][min_pos] = b'O';
+                            platform[row][col] = b'.';
+                            platform[row][min_pos] = b'O';
                             min_pos = min_pos.saturating_sub(1);
                         },
                         _ => continue
                     }
                 }
             }
-            let mut total_load = 0;
-            for row in 0..n {
-                for col in 0..m {
-                    if lines[row][col]==b'O' {
-                        total_load += (n - row) as i32;
-                    }
+            platform
+        }
+       
+        let mut fast_platform = platform.clone();
+        for current_rotation in 1..=num_rotations {
+            platform = rotate(platform);
+            fast_platform = rotate(rotate(fast_platform));
+            if platform==fast_platform {
+                let mut cycle_len = 1;
+                while {
+                    fast_platform = rotate(fast_platform);
+                    platform!=fast_platform
+                } {
+                    cycle_len += 1;
                 }
+                let remained_rotations = (num_rotations - current_rotation) % cycle_len;
+                for _ in 0..remained_rotations {
+                    platform = rotate(platform);
+                }
+                break;
             }
-            println!("{}: {}", cycle, total_load)
-
-            // let mut platform = Vec::new();
-            // for line in lines.iter() {
-            //     platform.push(String::from_utf8(line.clone()).unwrap());
-            // }
-            // for platform_line in platform {
-            //     println!("{platform_line:?}");
-            // }
-            // println!();
         }
 
+        let (n, m) = (platform.len(), platform[0].len());
         let mut total_load = 0;
         for row in 0..n {
             for col in 0..m {
-                if lines[row][col]==b'O' {
+                if platform[row][col]==b'O' {
                     total_load += (n - row) as i32;
                 }
             }
         }
         total_load
     }
-
-    
 }
 
 #[cfg(test)]
@@ -134,8 +137,7 @@ mod tests {
     #[tokio::test]
     async fn solve2() {
         let lines = load_lines("day_14.txt").await.unwrap();
-        let result = Solution::calculate_load_in_cycles(lines.iter().map(|ln| Vec::from(ln.as_bytes())).collect::<Vec<_>>(), 1000);
-//        let result = Solution::calculate_load_in_cycles(lines.iter().map(|ln| Vec::from(ln.as_bytes())).collect::<Vec<_>>(), 1_000_000_000);
+        let result = Solution::calculate_load_in_cycles(lines.iter().map(|ln| Vec::from(ln.as_bytes())).collect::<Vec<_>>(), 1_000_000_000);
         println!("{result:?}");
     }
 }
