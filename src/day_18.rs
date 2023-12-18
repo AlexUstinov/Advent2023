06b917478195b5068548ef446c19074c80ed761d
Aleksandr Ustinov <ustinovas@gmail.com>
2023-12-18 13:43:41 +0000

    Rewrite solution of the 18th day first problem.

--- src/day_18.rs
+++ src/day_18.rs
@@ -1,3 +1,6 @@
+use std::collections::BTreeMap;
+use std::ops::Bound::{Included, Excluded};
+
 pub struct Solution;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
@@ -31,51 +34,90 @@ impl Solution {
             }
         }
 
-        let (x0, y0) = (min_h.abs() as usize, min_v.abs() as usize);
-        let height = (max_v-min_v+1) as usize;
-        let mut field = vec![vec![]; height];
+        let (mut x, mut y) = (min_h.abs(), min_v.abs());
+        let mut field = BTreeMap::new();
+        for (dir, len) in commands.iter() {
+            match dir {
+                Dir::U => y-=len,
+                Dir::R => x+=len,
+                Dir::D => y+=len,
+                Dir::L => x-=len,
+            }
+            _ = field.entry(y).or_insert_with(|| (1, Vec::new()));
+        }
+
+        let mut expander_rows = Vec::new();
+        for (y1, y2) in field.keys().zip(field.keys().skip(1)) {
+            let size = y2-y1-1;
+            if size > 0 {
+                expander_rows.push((y1+1, size));
+            }
+        }
+        for &(pos, size) in expander_rows.iter() {
+            _ = field.entry(pos).or_insert_with(|| (size, Vec::new()));
+        }
+
+        // x and y must be at the starting position now
+
+        let mut d_len = 0;
         let first_cmd = &commands[0];
         let commands_iter = commands.iter().zip(commands.iter().skip(1).chain(std::iter::once(first_cmd)));
-        let (mut x, mut y) = (x0, y0);
         for (dir, next_dir, len) in commands_iter.map(|(&(dir, len), &(next_dir, _))| (dir, next_dir, len)) {
-            for _ in 0..len {
-                match dir {
-                    Dir::U => y-=1,
-                    Dir::R => x+=1,
-                    Dir::D => y+=1,
-                    Dir::L => x-=1,
+            d_len += len;
+            let dir_is_vertical = dir==Dir::U || dir==Dir::D;
+            let next_dir_is_vertical = next_dir==Dir::U || next_dir==Dir::D;
+            if dir_is_vertical && next_dir_is_vertical {
+                continue;
+            }
+            let prev_y = y;
+            match dir {
+                Dir::U => y-=d_len,
+                Dir::R => x+=d_len,
+                Dir::D => y+=d_len,
+                Dir::L => x-=d_len,
+            }
+            if dir_is_vertical && !next_dir_is_vertical {
+                let cell_dir = if next_dir==Dir::R { [dir, Dir::R] } else { [Dir::L, dir ] };
+                if let Some((_, v_crossings)) = field.get_mut(&y) {
+                    v_crossings.push((x, cell_dir))
                 }
-                let dir_is_vertical = dir==Dir::U || dir==Dir::D;
-                let next_dir_is_vertical = next_dir==Dir::U || next_dir==Dir::D;
-                if dir_is_vertical || next_dir_is_vertical {
-                    let v_dir = if dir_is_vertical { dir } else { next_dir };
-                    field[y].push((x, v_dir));
+            } else if !dir_is_vertical && next_dir_is_vertical {
+                let cell_dir = if dir==Dir::R { [Dir::L, next_dir] } else { [next_dir, Dir::R] };
+                if let Some((_, v_crossings)) = field.get_mut(&y) {
+                    v_crossings.push((x, cell_dir))
+                }
+
+            }
+            if dir_is_vertical {
+                for (_, (_, v_crossings)) in field.range_mut((Excluded(prev_y.min(y)), Excluded(prev_y.max(y)))) {
+                    v_crossings.push((x, [dir, dir]));
                 }
             }
+            d_len = 0;
         }
 
-        for row in field.iter_mut() {
-            row.sort_unstable();
+        for (_, v_crossings) in field.values_mut() {
+            v_crossings.sort_unstable();
         }
 
-        let interior_is_to_the_right = matches!(field[0][0], (_, Dir::U));
         let mut count = 0;
-        for row in field {
+        let interior_is_to_the_right = matches!(field.first_key_value().map(|(_, (_, v_crossings))| v_crossings[0]).unwrap(), (_, [Dir::U, _]));
+        for (size, v_crossings) in field.values() {
+            let size = *size;
             let mut is_interior = false;
-            let (mut prev_x, mut prev_v_dir) = (0, Dir::R);
-            for (x, v_dir) in row {
-                count += 1;
-                let shifted_cross = v_dir==prev_v_dir;
-                if is_interior || shifted_cross {
+            let (mut prev_x, mut prev_dir) = (0, [Dir::R, Dir::U]);
+            for &(x, dir) in v_crossings {
+                count += size as i64;
+                let is_horizontal_border = matches!((prev_dir[1], dir[0]), (Dir::R, Dir::L));
+                if is_interior || is_horizontal_border {
                     let delta = x - prev_x - 1;
-                    count += delta;
+                    count += (delta as i64) * (size as i64);
                 }
-                is_interior = interior_is_to_the_right ^ (v_dir==Dir::D);
+                is_interior = interior_is_to_the_right ^ (dir[1]==Dir::D || dir[0]==Dir::D);
                 prev_x = x;
-                prev_v_dir = v_dir;
+                prev_dir = dir;
             }
         }
-
         count as i64
     }
 
